b28a437cc2e7c6dce8e7ac15a59d412fda9d8334
Nazareno <nazarenorosales_@hotmail.com>
2024-01-11 19:57:06 -0300

    New meno mobile, and resoponsive design. And adding font awesome

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,50 @@
-import styles from "./NavBar.module.css"
+import styles from "./NavBar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
 export default function NavBar() {
-    return (
-        <nav className={styles.navbar}>
-            <ul>
-                <li> <a href="/proyectos">Proyectos</a></li>
-                <li> <a href="/contacto">Contacto</a></li>
-                <li> <a href="/sobre-mi">Sobre mi</a></li>
+  const [menuVisible, setMenuVisible] = useState(false);
+  const handleMenuClick = () => {
+    setMenuVisible(!menuVisible);
+  };
+  return (
+    <>
+      <div className={styles.menuMobile}>
+        <div className= {styles.mobileElements}>
+          <FontAwesomeIcon
+            icon={faBars}
+            className={styles.menuIcon}
+            onClick={handleMenuClick}
+          />
+          <nav>
+            <ul style={{ display: menuVisible ? "block" : "none" }}>
+              <li>
+                <a href="/proyectos">Proyectos</a>
+              </li>
+              <li>
+                <a href="/contacto">Contacto</a>
+              </li>
+              <li>
+                <a href="/sobre-mi">Sobre mi</a>
+              </li>
             </ul>
-        </nav>
-    )
+          </nav>
+        </div>
+      </div>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            <a href="/proyectos">Proyectos</a>
+          </li>
+          <li>
+            <a href="/contacto">Contacto</a>
+          </li>
+          <li>
+            <a href="/sobre-mi">Sobre mi</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
